b5a909f932bd0016c20f07af3808d52b75b94a57
Revan2005 <tomek.biegus@gmail.com>
2015-12-21 16:32:15 +0100

    jest okienko menu na poczatku sluzace do ustawienia parametrow, znika po kliknieciu start, parametry szybkosci klikania i przyspieszenia sa odwrotne do intuicji - im wieksze tym wolniej, poza tym kamera adaptuje sie do oswietlenia zmieniajac rozmiar ramki akwizycji a ja zliczam klatki wiec kiedy jest ciemno n klatek to wiecej czasu nic ta sama liczba klatek kiedy jest jasno, przydalo by sie mierzyc czas w sekundach jednak

--- czwartaIdea/Start.py
+++ czwartaIdea/Start.py
@@ -1,21 +1,35 @@
 import os
+from Tkinter import  *
+import tkMessageBox
+
+if __name__ == "__main__":
+    top = Tk()
+    
+    widthTrackbar = Scale(top, from_ = 5, to = 200, orient = HORIZONTAL, label = "Szerokosc ramki")
+    widthTrackbar.set(25)
+    widthTrackbar.pack()
+    heightTrackbar = Scale(top, from_ = 5, to = 200, orient = HORIZONTAL, label = "Wysokosc ramki")
+    heightTrackbar.set(15)
+    heightTrackbar.pack()
+    accelerationTrackbar = Scale(top, from_ = 1, to = 10, orient = HORIZONTAL, label = "Przyspieszenie do -1")
+    accelerationTrackbar.set(1)
+    accelerationTrackbar.pack()
+    clickTrackbar = Scale(top, from_ = 1, to = 100, orient = HORIZONTAL, label = "Szybkosc klikania do -1")
+    clickTrackbar.set(150)
+    clickTrackbar.pack()
+    
+    def start():
+        ramkaWidth = widthTrackbar.get()
+        ramkaHeight = heightTrackbar.get()
+        LICZBA_KLATEK_DO_PRZYSPIESZENIA = accelerationTrackbar.get()
+        LICZBA_KLATEK_POTRZEBNA_DO_AKTYWACJI = clickTrackbar.get()
+        top.destroy()
+        os.system("python EyeTracking.py " + str(ramkaWidth) + " " + str(ramkaHeight) + 
+                  " " + str(LICZBA_KLATEK_DO_PRZYSPIESZENIA) + " " + str(LICZBA_KLATEK_POTRZEBNA_DO_AKTYWACJI))
+        
+    B = Button(top, text ="Uruchom", command = start)
+    B.pack()
+    top.mainloop()
 
 
-ramkaWidth = 250
-ramkaHeight = 150
-gruboscObszaruSterowaniaKursorem = 2;
 
-#dlugoscObszaruKlikania = ramkaWidth - 2*gruboscObszaruSterowaniaKursorem
-#wysokoscObszaruKlikania = ramkaHeight - 2*gruboscObszaruSterowaniaKursorem
-
-
-
-LICZBA_KLATEK_POTRZEBNA_DO_AKTYWACJI = 150
-
-
-lICZBA_KLATEK_DO_PRZYSPIESZENIA = 3
-maxSpeed = 100
-
-
-os.system("python EyeTracking.py " + str(ramkaWidth) + " " + str(ramkaHeight) + 
-          " " + str(lICZBA_KLATEK_DO_PRZYSPIESZENIA) + " " + str(LICZBA_KLATEK_POTRZEBNA_DO_AKTYWACJI))
